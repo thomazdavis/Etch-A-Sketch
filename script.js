3f3468076f2c9ec83fab559ee989064f301182d5
Thomas Davis <thomas.medavis@gmail.com>
2025-01-29 12:43:20 -0700

    OdinEAS-2: Hover to change background

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const gridSize = 16;
 for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
+    
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    });
+
     container.appendChild(square);
+
 }
